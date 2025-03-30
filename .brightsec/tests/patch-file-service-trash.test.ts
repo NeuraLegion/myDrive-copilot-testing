0c71ae15732745c81e0886e957affc76573bb5df
Bar Hofesh <bar.hofesh@gmail.com>
2025-03-30 14:47:00 +0300

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/patch-file-service-trash.test.ts
@@ -0,0 +1,35 @@
+import { test, before, after } from 'node:test';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+import { SecRunner } from '@sectester/runner';
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+test('PATCH /file-service/trash', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'bopla', 'mass_assignment', 'excessive_data_exposure', 'id_enumeration'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.PATCH,
+      url: `${baseUrl}/file-service/trash`,
+      body: JSON.stringify({ id: "exampleFileID" }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+});
